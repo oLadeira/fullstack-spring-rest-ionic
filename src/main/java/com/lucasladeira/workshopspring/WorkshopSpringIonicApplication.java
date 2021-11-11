842f368041995259964539f97f9db2907461fd9a
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-11 11:38:12 -0300

    Project created

---
+++ src/main/java/com/lucasladeira/workshopspring/WorkshopSpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.lucasladeira.workshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasladeira/workshopspring/WorkshopSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasladeira.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
