6eb2d5495db944311cccfa7f2c9ef3a72346f8e9
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-19 11:17:58 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/lucasladeira/workshopspring/resources/exceptions/ValidationError.java
@@ -0,0 +1,27 @@
+package com.lucasladeira.workshopspring.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends BodyError {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
