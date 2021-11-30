d4936bdb98392e056799ee129014b61d1d61ccd6
Lucas Ladeira <lucas-ladeira@hotmail.com>
2021-11-30 10:33:50 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

---
+++ src/main/java/com/lucasladeira/workshopspring/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.lucasladeira.workshopspring.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.lucasladeira.workshopspring.domain.Cliente;
+import com.lucasladeira.workshopspring.dto.ClienteDTO;
+import com.lucasladeira.workshopspring.repositories.ClienteRepository;
+import com.lucasladeira.workshopspring.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request; //usado pra requisitar a url do ClienteDTO
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
